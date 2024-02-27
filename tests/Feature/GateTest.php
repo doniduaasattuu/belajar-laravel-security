c853d3d3ce17c97d3b5f893b72863dfe637d2190
doniduaasattuu <doni.duaasattuu@gmail.com>
2024-02-27 14:13:57 +0700

    gate facade

--- tests/Feature/GateTest.php
+++ tests/Feature/GateTest.php
@@ -42,4 +42,39 @@ class GateTest extends TestCase
         self::assertFalse(Gate::allows('update-contact', $contact));
         self::assertFalse(Gate::allows('delete-contact', $contact));
     }
+
+    public function testGateMethod()
+    {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+        $user = User::where('email', 'doni@localhost')->firstOrFail();
+        Auth::login($user);
+
+        $contact = Contact::where('email', 'test@localhost')->firstOrFail();
+        self::assertTrue(Gate::any(['get-contact', 'update-contact', 'delete-contact'], $contact));
+        self::assertFalse(Gate::none(['get-contact', 'update-contact', 'delete-contact'], $contact));
+
+        $user =  User::where('email', 'eko@localhost')->firstOrFail();
+        Auth::login($user);
+
+        self::assertFalse(Gate::any(['get-contact', 'update-contact', 'delete-contact'], $contact));
+        self::assertTrue(Gate::none(['get-contact', 'update-contact', 'delete-contact'], $contact));
+    }
+
+    public function testGateNonLogin()
+    {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+
+        $user = User::where('email', 'doni@localhost')->firstOrFail();
+        $contact = Contact::where('email', 'test@localhost')->firstOrFail();
+
+        // JIKA USER TIDAK LOGIN OTOMATIS METHOD ALLOWS MENGEMBALIKAN FALSE
+        self::assertFalse(Gate::allows('get-contact', $contact));
+        self::assertFalse(Gate::allows('update-contact', $contact));
+        self::assertFalse(Gate::allows('delete-contact', $contact));
+
+        $gate = Gate::forUser($user);
+        self::assertTrue($gate->allows('get-contact', $contact));
+        self::assertTrue($gate->allows('update-contact', $contact));
+        self::assertTrue($gate->allows('delete-contact', $contact));
+    }
 }
